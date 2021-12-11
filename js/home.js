a788be34d6f51ed5acf5124afb196b07019f977b
saranyellanki <saran.yellanki12@gmail.com>
2021-12-11 07:34:10 +0530

    Added: delete button functionality

--- js/home.js
+++ js/home.js
@@ -1,35 +1,46 @@
+let addressBookList;
 window.addEventListener('DOMContentLoaded', (event) => {
     addressBookList = getAddressBookData();
     document.querySelector('.person-count').textContent = addressBookList.length;
     createInnerHtml();
-})
+});
 
 const getAddressBookData = () => {
     if(localStorage.getItem('AddressBookList') != undefined) {
         return JSON.parse(localStorage.getItem('AddressBookList'));
     }else {
-        return [];
+        return;
     }
 }
 
 const createInnerHtml = () => {
     const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Actions</th>"
     let innerHtml = `${headerHtml}`;
-    for (const addressBookData of addressBookList) {
+    for (const addressBook of addressBookList) {
         innerHtml = `${innerHtml}
     <tr>
-        <td>${addressBookData._name}</td>
-        <td class="address-block">${addressBookData._address}</td>
-        <td>${addressBookData._city}</td>
-        <td>${addressBookData._state}</td>
-        <td>${addressBookData._zipcode}</td>
-        <td>${addressBookData._phone}</td>
+        <td>${addressBook._name}</td>
+        <td class="address-block">${addressBook._address}</td>
+        <td>${addressBook._city}</td>
+        <td>${addressBook._state}</td>
+        <td>${addressBook._zipcode}</td>
+        <td>${addressBook._phoneNumber}</td>
         <td>
-            <img id="1" onclick="remove()" alt="delete" src="../assets/delete-black-18dp.svg">
-            <img id="1" alt="edit" onclick="update()" src="../assets/create-black-18dp.svg">
+            <img id="${addressBook._name}" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
+            <img id="${addressBook._name}" alt="edit" onclick="update()" src="../assets/create-black-18dp.svg">
         </td>
     </tr>
     `;
         document.querySelector('#display').innerHTML = innerHtml;
     }
+}
+
+remove = (node) => {
+    let addressBookData = addressBookList.find(contact => contact._name == node.id);
+    if(!addressBookData) return;
+    const contactIndex = addressBookList.map(contactName => contactName._name).indexOf(addressBookData._name);
+    addressBookList.splice(contactIndex,1);
+    localStorage.setItem("AddressBookList",JSON.stringify(addressBookList));
+    document.querySelector('.person-count').textContent = addressBookList.length;
+    createInnerHtml();
 }
