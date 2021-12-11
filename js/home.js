cadcd0953d62cb3136cf2e9e9975d1e237dd1aef
saranyellanki <saran.yellanki12@gmail.com>
2021-12-11 06:41:30 +0530

    Added: Address Book list from local storage

--- js/home.js
+++ js/home.js
@@ -1,42 +1,35 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    addressBookList = getAddressBookData();
+    document.querySelector('.person-count').textContent = addressBookList.length;
     createInnerHtml();
 })
 
+const getAddressBookData = () => {
+    if(localStorage.getItem('AddressBookList') != undefined) {
+        return JSON.parse(localStorage.getItem('AddressBookList'));
+    }else {
+        return [];
+    }
+}
+
 const createInnerHtml = () => {
-    const innerHtml = `
+    const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th>Actions</th>"
+    let innerHtml = `${headerHtml}`;
+    for (const addressBookData of addressBookList) {
+        innerHtml = `${innerHtml}
     <tr>
-        <th>Fullname</th>
-        <th>Address</th>
-        <th>City</th>
-        <th>State</th>
-        <th>Zip Code</th>
-        <th>Phone Number</th>
-        <th>Actions</th>
-    </tr>
-    <tr>
-        <td>Saran Yellanki</td>
-        <td>FNo 103 Vinayaka Plaza Kurmannapalem</td>
-        <td>Visakhapatnam</td>
-        <td>Andhra Pradesh</td>
-        <td>9618058101</td>
-        <td>530046</td>
-        <td>
-            <img id="1" onclick="remove()" alt="delete" src="../assets/delete-black-18dp.svg">
-            <img id="1" alt="edit" onclick="update()" src="../assets/create-black-18dp.svg">
-        </td>
-    </tr>
-    <tr>
-        <td>Dinesh Bodepalli</td>
-        <td>Nad Lane number 7, Simhachalam</td>
-        <td>Visakhapatnam</td>
-        <td>Andhra Pradesh</td>
-        <td>8437473059</td>
-        <td>530032</td>
+        <td>${addressBookData._name}</td>
+        <td class="address-block">${addressBookData._address}</td>
+        <td>${addressBookData._city}</td>
+        <td>${addressBookData._state}</td>
+        <td>${addressBookData._zipcode}</td>
+        <td>${addressBookData._phone}</td>
         <td>
             <img id="1" onclick="remove()" alt="delete" src="../assets/delete-black-18dp.svg">
             <img id="1" alt="edit" onclick="update()" src="../assets/create-black-18dp.svg">
         </td>
     </tr>
     `;
-    document.querySelector('#display').innerHTML = innerHtml;
+        document.querySelector('#display').innerHTML = innerHtml;
+    }
 }
