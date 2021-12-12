c14152325c11e39af19f40bdea02889cf9b74b0a
saranyellanki <saran.yellanki12@gmail.com>
2021-12-12 23:22:45 +0530

    Added: read data from local storage and update form

--- js/home.js
+++ js/home.js
@@ -3,13 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
     addressBookList = getAddressBookData();
     document.querySelector('.person-count').textContent = addressBookList.length;
     createInnerHtml();
+    localStorage.removeItem('editContact');
 });
 
 const getAddressBookData = () => {
     if(localStorage.getItem('AddressBookList') != undefined) {
         return JSON.parse(localStorage.getItem('AddressBookList'));
     }else {
-        return;
+        return [];
     }
 }
 
@@ -27,7 +28,7 @@ const createInnerHtml = () => {
         <td>${addressBook._phoneNumber}</td>
         <td>
             <img id="${addressBook._name}" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
-            <img id="${addressBook._name}" alt="edit" onclick="update()" src="../assets/create-black-18dp.svg">
+            <img id="${addressBook._name}" alt="edit" onclick="update(this)" src="../assets/create-black-18dp.svg">
         </td>
     </tr>
     `;
@@ -43,4 +44,11 @@ remove = (node) => {
     localStorage.setItem("AddressBookList",JSON.stringify(addressBookList));
     document.querySelector('.person-count').textContent = addressBookList.length;
     createInnerHtml();
+}
+
+update = (node) => {
+    let addressBookData = addressBookList.find(contact => contact._name == node.id);
+    if(!addressBookData) return;
+    localStorage.setItem("editContact",JSON.stringify(addressBookData));
+    window.location.replace("../pages/AddressBookApp.html")
 }
